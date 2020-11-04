3ce75a7a5a52fd63968c029c736ae6bc4bb3c0e6
hadi <hadidin4423@gmail.com>
2020-11-04 17:17:55 +0800

    fix failed to gen cert

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(sss.rpi-iot.tk)
+domains=(rpi-iot.duckdns.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="rpi.iot.2020@gmail.com" # Adding a valid address is strongly recommended
